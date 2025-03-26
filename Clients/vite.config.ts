7dd00a5f04995524f56f58be6ab6f708e0794c10
Samuel Coutinho <sfcrp2000@gmail.com>
2025-03-25 22:43:43 -0400

    Update @types/node to version 22.13.13 and refactor Vite configuration to load environment variables

--- Clients/vite.config.ts
+++ Clients/vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    host: "0.0.0.0",
-  },
-  define: { global: "globalThis", 'import.meta.env.VITE_APP_API_BASE_URL': JSON.stringify(process.env.VITE_APP_API_BASE_URL) }
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   server: {
+//     host: "0.0.0.0",
+//   },
+//   define: { global: "globalThis", 'import.meta.env.VITE_APP_API_BASE_URL': JSON.stringify(process.env.VITE_APP_API_BASE_URL) }
+// });
+
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  // Load environment variables manually
+  dotenv.config();
+
+  return {
+    plugins: [react(), svgr()],
+    server: {
+      host: "0.0.0.0",
+    },
+    define: {
+      'import.meta.env.VITE_APP_API_BASE_URL': JSON.stringify(process.env.VITE_APP_API_BASE_URL),
+    },
+  };
 });
